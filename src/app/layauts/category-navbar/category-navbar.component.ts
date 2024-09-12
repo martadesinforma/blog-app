d3a11f84238e37eeaeee6c081c18e6a5fcc6423f
Marta Ocana <martacyta2021@gmail.com>
2024-09-12 18:46:42 +0200

    update category-navbar component

--- src/app/layauts/category-navbar/category-navbar.component.ts
+++ src/app/layauts/category-navbar/category-navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from '../../services/categories.service';
 
 @Component({
   selector: 'app-category-navbar',
   templateUrl: './category-navbar.component.html',
   styleUrl: './category-navbar.component.css'
 })
-export class CategoryNavbarComponent {
+export class CategoryNavbarComponent implements OnInit{
+
+  categoryArray: Array<any> = []; //Se veria de esta forma: [{data: {category: 'hola'} id: "m1VjCvgL6PFXULVTyvKN" }, {data: {category: 'uri'} id: "Zotz2wMaJwVVZn1J79Ws" },  ]
+
+  constructor(private categoryService: CategoriesService){}
+
+  ngOnInit(): void {
+    this.categoryService.loadData()
+      .subscribe(val => {
+        this.categoryArray = val;
+      })
+  }
 
 }
