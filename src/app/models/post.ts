bd12dc2cee59336beb8cc3b269b92a4e51ecbebb
Marta Ocana <martacyta2021@gmail.com>
2024-09-12 18:47:57 +0200

    create post interface

---
+++ src/app/models/post.ts
@@ -0,0 +1,18 @@
+import { Timestamp } from "@angular/fire/firestore";
+
+export interface Post {
+  id?:string;
+  title: string,
+  permalink: string,
+  category: {
+    categoryId: string,
+    category: string,
+  },
+  postImgPath: string,
+  excerpt: string,
+  content: string,
+  isFeatured: boolean,
+  views: number,
+  status: string,
+  createdAt: Timestamp,
+}
