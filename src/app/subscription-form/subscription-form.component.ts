062327ff0e246c9bf9566040f32a6fa6462a80f0
Marta Ocana <martacyta2021@gmail.com>
2024-09-16 18:23:51 +0200

    update subscription form compoenent

--- src/app/subscription-form/subscription-form.component.ts
+++ src/app/subscription-form/subscription-form.component.ts
@@ -27,6 +27,7 @@ export class SubscriptionFormComponent {
         if (val.empty) { //Si val.empty es true, significa que la consulta a Firestore no encontró ningún documento con el correo electrónico proporcionado en la colección de subscribers. Esto indica que el correo ingresado es nuevo (no existe en la base de datos).
           this.subscribersService.addSubs(subscriberData); //si no existe ningún documento con el correo electrónico proporcionado en la colección de subscribers, quiero que me lo cree con el correo y el nombre ingresados.
           this.isSubscribed = true;
+          this.isEmailError = false;
         } else {//Si val.empty es false, significa que la consulta a Firestore si encontró un documento con el correo electrónico  en la colección de subscribers. Esto indica que el correo ingresado no es nuevo (ya existe en la base de datos).
           this.isEmailError = true;
         }
