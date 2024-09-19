b277b32041115c794ddad33fadb07c8253e2b2ae
Marta Ocana <martacyta2021@gmail.com>
2024-09-19 16:56:58 +0200

    update comment-form component

--- src/app/comments/comment-form/comment-form.component.ts
+++ src/app/comments/comment-form/comment-form.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommentsService } from '../../services/comments.service';
+import { Comment } from '../../models/comment';
+import { Timestamp } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-comment-form',
@@ -7,4 +10,24 @@ import { Component } from '@angular/core';
 })
 export class CommentFormComponent {
 
+  @Input()
+  docId: string = ''; //id del post  donde estas escribiendo el comment. Lo recibe de singlePostComponent
+
+  constructor(private commentsService: CommentsService) {
+
+
+  }
+
+  onSubmit(commentForm: any){
+    const commentData: Comment = {
+      name: commentForm.value.name,
+      comment: commentForm.value.comment,
+      postId: this.docId, //id del post  donde estas escribiendo el comment
+      createdAt: Timestamp.fromDate(new Date())
+    }
+
+    this.commentsService.addComments(commentData);
+
+    commentForm.reset();
+  }
 }
